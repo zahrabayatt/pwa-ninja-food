a511fb4b920b55ad2e53a41ef8591f304f30c195
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-18 03:11:03 +0330

    Pre-caching assets

--- sw.js
+++ sw.js
@@ -1,6 +1,28 @@
+const staticCacheName = "site-static";
+// assets are array of path resources
+const assets = [
+  "/", // index.html
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+];
+
 // listening to install service worker event
 self.addEventListener("install", (event) => {
-  console.log("Service worker has been installed");
+  // it might only take a spilt second for the service worker to be installed and after installation if service worker do nothing the browser stopped it and to avoid this problem we use event.waitUntil
+  event.waitUntil(
+    // open api: open the cache if exist and if not exist, create it and then open it:
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets...");
+      // to add items to cache we use add or addAll
+      cache.addAll(assets);
+    })
+  );
 });
 
 // listening to activate service worker event
@@ -8,7 +30,7 @@ self.addEventListener("activate", (event) => {
   console.log("Service worker has been activated");
 });
 
-// listening to fetch service worker event
+// listening to fetch event
 self.addEventListener("fetch", (event) => {
   console.log("fetch event", event);
 });
