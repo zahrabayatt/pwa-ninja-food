28787bebb22246c9738caabcda7335795b565006
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-18 03:31:22 +0330

    Getting cached assets

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 const staticCacheName = "site-static";
-// assets are array of path resources
 const assets = [
-  "/", // index.html
+  "/",
   "/index.html",
   "/js/app.js",
   "/js/ui.js",
@@ -10,16 +9,14 @@ const assets = [
   "/css/materialize.min.css",
   "img/dish.png",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v142/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
 ];
 
 // listening to install service worker event
 self.addEventListener("install", (event) => {
-  // it might only take a spilt second for the service worker to be installed and after installation if service worker do nothing the browser stopped it and to avoid this problem we use event.waitUntil
   event.waitUntil(
-    // open api: open the cache if exist and if not exist, create it and then open it:
     caches.open(staticCacheName).then((cache) => {
       console.log("caching shell assets...");
-      // to add items to cache we use add or addAll
       cache.addAll(assets);
     })
   );
@@ -32,5 +29,10 @@ self.addEventListener("activate", (event) => {
 
 // listening to fetch event
 self.addEventListener("fetch", (event) => {
-  console.log("fetch event", event);
+  // explanation: https://youtu.be/0mAw9Na6hyM?si=nlVGVNrL3LvszX1R
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request);
+    })
+  );
 });
