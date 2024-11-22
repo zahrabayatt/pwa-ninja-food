5422672331168f85b651639f5264d4735b2d8542
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-22 15:31:13 +0330

    Updating the UI

--- js/db.js
+++ js/db.js
@@ -1,11 +1,9 @@
 // real-time listener
 db.collection("recipes").onSnapshot((snapshot) => {
-  // console.log(snapshot.docChanges())
   snapshot.docChanges().forEach((change) => {
-    // console.log(change._delegate, change.doc.data(), change.doc.id);
-
     if (change.type === "added") {
       // add the document dat to the web page
+      renderRecipe(change.doc.data(), change.doc.id);
     }
 
     if (change.type === "removed") {
